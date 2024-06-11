021eb7e82f1feac00fd823f541fdeea1d4ed67b6
wnsgh12s <junho1273@gmail.com>
2024-06-11 17:56:36 +0900

    5622번 다이얼

---
+++ src/다이얼.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class 다이얼 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.next();
+
+        //ABC, DEF , GHI... 등등 알파뱃 순서로 맵핑
+        int[] nums = {2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 9};
+
+        int result = 0;
+        for (char c : input1.toCharArray()) {
+            result += nums[c - 'A'] + 1;
+        }
+
+        System.out.println(result);
+    }
+}
